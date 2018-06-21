dbee4036bbb2d012a363269308de53afe06375cc
Victor Shamanov <wiruzx@gmail.com>
2018-06-21 20:30:09 +0100

    Split ElementsViewController.swift file

--- Gallery/Internal/Controllers/Elements/ElementsViewController.swift
+++ Gallery/Internal/Controllers/Elements/ElementsViewController.swift
@@ -24,8 +24,6 @@
 
 import UIKit
 
-// MARK: - Elements View Controller
-
 final class ElementsViewController: UIViewController, UICollectionViewDataSource {
 
     // MARK: - Private properties
@@ -73,86 +71,6 @@ final class ElementsViewController: UIViewController, UICollectionViewDataSource
     }
 }
 
-// MARK: - Cell
-
-private final class ElementCollectionViewCell: UICollectionViewCell {
-
-    // MARK: - Private properties
-
-    private let titleLabel: UILabel
-    private let container: UIView
-
-    // MARK: - Instantiation
-
-    override init(frame: CGRect) {
-        titleLabel = UILabel()
-        container = UIView()
-        super.init(frame: .zero)
-
-        titleLabel.textAlignment = .center
-
-        for view in [titleLabel, container] {
-            view.translatesAutoresizingMaskIntoConstraints = false
-            addSubview(view)
-        }
-
-        NSLayoutConstraint.activate([
-            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
-            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
-            titleLabel.topAnchor.constraint(equalTo: topAnchor),
-            container.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
-            container.leadingAnchor.constraint(equalTo: leadingAnchor),
-            container.trailingAnchor.constraint(equalTo: trailingAnchor),
-            container.bottomAnchor.constraint(equalTo: bottomAnchor)
-        ])
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    // MARK: - UICollectionViewCell
-
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        container.subviews.forEach { $0.removeFromSuperview() }
-    }
-
-    // MARK: - API
-
-    func setElement(_ element: Element) {
-        titleLabel.text = element.title
-
-        let view = element.view
-        view.translatesAutoresizingMaskIntoConstraints = false
-        container.addSubview(view)
-
-        var constraints = view.constraintsForAligningAllEdgesWithSuperview()
-
-        if let width = element.width {
-            constraints.append(view.widthAnchor.constraint(equalToConstant: width))
-        }
-
-        if let height = element.height {
-            constraints.append(view.heightAnchor.constraint(equalToConstant: height))
-        }
-
-        NSLayoutConstraint.activate(constraints)
-    }
-}
-
-// MARK: - Layout
-
-private extension UICollectionViewLayout {
-    static func makeElementsFlowLayout() -> UICollectionViewFlowLayout {
-        let layout = UICollectionViewFlowLayout()
-        layout.estimatedItemSize = UICollectionViewFlowLayoutAutomaticSize
-        layout.sectionInset = UIEdgeInsets(top: 15, left: 0, bottom: 15, right: 0)
-        layout.minimumLineSpacing = 30
-        layout.minimumInteritemSpacing = 7
-        return layout
-    }
-}
 
 // MARK: - Utility extensions
 
